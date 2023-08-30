78af317a51129de49ceee9982a419d8f138f901e
Armynerh <aimenah@outlook.com>
2023-08-30 19:01:46 +0100

    Modify booksSlice, used spread operator to make a copy of state

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,21 +1,31 @@
-import { v4 as uuidv4 } from 'uuid';
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  books: [],
-};
+const initialState = [{
+  item_id: 'item1',
+  title: 'The Great Gatsby',
+  author: 'John Smith',
+  category: 'Fiction',
+},
+{
+  item_id: 'item2',
+  title: 'Anna Karenina',
+  author: 'Leo Tolstoy',
+  category: 'Fiction',
+},
+{
+  item_id: 'item3',
+  title: 'The Selfish Gene',
+  author: 'Richard Dawkins',
+  category: 'Nonfiction',
+}];
 const booksSlice = createSlice({
   name: 'books',
   initialState,
-  reducers: {
-    addBook: (state, action) => {
-      state.books.push({ id: uuidv4(), title: action.payload });
-    },
-    removeBook: (state, action) => {
-      state.tasks = state.booksList.filter((book) => book.id !== action.payload);
-    },
-  },
 
+  reducers: {
+    addBook: (state, action) => [...state, action.payload],
+    removeBook: (state, action) => state.filter((book) => book.item_id !== action.payload),
+  },
 });
 export const { addBook, removeBook } = booksSlice.actions;
 
